69e66e26eb9085f4868cb312b496d89283df7c3e
Your Dennis <decond234@gmail.com>
2026-03-02 14:51:10 +0100

    Debug: log join_community requests to php-cli-test/join_debug.log

--- xhr/join_community.php
+++ xhr/join_community.php
@@ -1,6 +1,11 @@
 <?php 
 
 if ($f == 'join_community') {
+    // Log join attempts for debugging Suggested Communities join failures
+    $log_path = dirname(__DIR__) . '/php-cli-test/join_debug.log';
+    $log_entry = "[" . date('c') . "] JOIN_ATTEMPT GET=" . json_encode($_GET) . " POST=" . json_encode($_POST) . " SESSION_main_hash=" . (isset($_SESSION['main_hash_id']) ? $_SESSION['main_hash_id'] : 'null') . "\n";
+    @file_put_contents($log_path, $log_entry, FILE_APPEND);
+
     if (isset($_GET['community_id']) && Wo_CheckMainSession($hash_id) === true) {
         $community_id = Wo_Secure($_GET['community_id']);
         if (Wo_IsCommunityJoined($community_id) === true || Wo_IsCommunityJoinRequested($community_id, $wo['user']['user_id']) === true) {
